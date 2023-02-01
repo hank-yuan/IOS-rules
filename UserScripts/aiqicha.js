9f711242cb6bafb3ec3424c4759595f61180924e
hank-yuan <hank-yuan@hotmail.com>
2023-02-01 11:25:10 +0800

    Update aiqicha.js

--- UserScripts/aiqicha.js
+++ UserScripts/aiqicha.js
@@ -4,11 +4,6 @@
 mitm= aiqicha.baidu.com
 */
 
-let obj = JSON.parse($response.body);
-    obj.data = {
-    "vip": 1,
-    "consume": 150,
-    "time": "2099-12-31",
-    "signInStaus": 0
-  }
-$done({body: JSON.stringify(obj)});
+var body = $response.body
+    .replace(/\"svip\":\{\"status\":0,\"startTime\":\"\",\"endTime\":\"\"/, "\"svip\":\{\"status\":1,\"startTime\":\"\",\"endTime\":\"2099-12-31\"");
+$done({ body });
