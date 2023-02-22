a06e60cff28c380fec4c11fd6316270bad63889d
hank-yuan <55567975+hank-yuan@users.noreply.github.com>
2023-02-23 01:39:58 +0800

    Update

--- UserScripts/12306.js
+++ UserScripts/12306.js
@@ -1,23 +1,5 @@
-/*
-12306去除倒计时
-[rewrite_local]
-^https?:\/\/ad\.12306\.cn\/ad\/ser\/getAdList url 12306.js
+# 使用说明：请在本地添加分流 host, ad.12306.cn, direct
 
-[mitm]
-hostname = ad.12306.cn
-*/
+const version = 'V1.0.22';
 
-
-var myobj = JSON.parse($response.body);
-if(myobj.materialsList){
-if(myobj.materialsList.length==1){
-myobj.materialsList[0].filePath="";
-myobj.advertParam.skipTime=1;
-myobj.advertParam.skipTimeAgain=5;
-myobj.advertParam.showSkipBtn=-1;
-}
-else if(myobj.materialsList.length>1){
-myobj.materialsList=[];
-}
-}
-$done({body: JSON.stringify(myobj)});
+var obj=JSON.parse($request.body),ddgksf2013={};"0007"==obj.placementNo?(ddgksf2013.materialsList=[{billMaterialsId:"6491",filePath:"ddgksf2013",creativeType:1}],ddgksf2013.advertParam={skipTime:1},ddgksf2013.code="00"):ddgksf2013="G0054"==obj.placementNo?{code:"00",materialsList:[{}]}:{code:"00",message:"无广告返回"},$done({body:JSON.stringify(ddgksf2013)});
