68afb98a2580e47bba6999dfaaaa8ffa63fd4ca3
hank-yuan <55567975+hank-yuan@users.noreply.github.com>
2024-08-27 13:34:32 +0800

    del

--- UserScripts/aiqicha.js
+++
@@ -1,9 +0,0 @@
-/*
-爱企查vip
-^https:\//aiqicha.baidu.com\/usercenter\/getvipinfoajax url script-response-body https://raw.githubusercontent.com/litieyin/AD_VIP/main/Script/aiqicha.js
-mitm= aiqicha.baidu.com
-*/
-
-var body = $response.body
-    .replace(/\"svip\":\{\"status\":0,\"startTime\":\"\",\"endTime\":\"\"/, "\"svip\":\{\"status\":1,\"startTime\":\"\",\"endTime\":\"2099-12-31\"");
-$done({ body });
