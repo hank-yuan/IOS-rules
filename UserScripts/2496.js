81c69e299a947342119dd3f80fae3ca1267d4a6e
hank-yuan <55567975+hank-yuan@users.noreply.github.com>
2025-08-24 03:47:38 +0800

    2496.js

--- UserScripts/2496.js
+++ UserScripts/2496.js
@@ -1,10 +1,10 @@
 /*
-圈X配置：
-
 [rewrite_local]
-^http?:\/\/api\_\d{4}\.kuwo\.cn\/front\/user\/vipstatus$ url script-response-body https://raw.githubusercontent.com/sngxpro/QuantumultX/master/2496/2496.js
+
+^http?:\/\/api\_\d{4}\.kuwo\.cn\/front\/user\/vipstatus$ url script-response-body https://raw.githubusercontent.com/hank-yuan/IOS-rules/master/UserScripts/2496.js
 
 [mitm]
+
 hostname = api_2496.kuwo.cn
 
 */
