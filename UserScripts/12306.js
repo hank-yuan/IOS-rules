18d402a00d7c64e4ee7a6e2fc3b43a6124df3f72
hank-yuan <55567975+hank-yuan@users.noreply.github.com>
2025-09-02 00:04:55 +0800

    del

--- UserScripts/12306.js
+++
@@ -1,5 +0,0 @@
-# 使用说明：请在本地添加分流 host, ad.12306.cn, direct
-
-const version = 'V1.0.22';
-
-var obj=JSON.parse($request.body),ddgksf2013={};"0007"==obj.placementNo?(ddgksf2013.materialsList=[{billMaterialsId:"6491",filePath:"ddgksf2013",creativeType:1}],ddgksf2013.advertParam={skipTime:1},ddgksf2013.code="00"):ddgksf2013="G0054"==obj.placementNo?{code:"00",materialsList:[{}]}:{code:"00",message:"无广告返回"},$done({body:JSON.stringify(ddgksf2013)});
